20a018e6f2d4b25a564ad1fd0b50c8fbcf5f630e
imjoseangel <josea.munoz@gmail.com>
2021-06-26 18:29:23 +0200

    add version

--- versions.tf
+++ versions.tf
@@ -2,12 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.63.0"
+      version = "~> 2.64.0"
     }
   }
   required_version = ">= 1.0"
-}
-
-provider "azurerm" {
-  features {}
 }
