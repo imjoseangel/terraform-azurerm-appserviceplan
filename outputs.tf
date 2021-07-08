344d1cb463e842ceff412b487c65d764264e8edf
imjoseangel <josea.munoz@gmail.com>
2021-07-08 13:14:58 +0200

    add output

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "app_service_plan_id" {
+output "id" {
   description = "Id of the created App Service Plan"
   value       = azurerm_app_service_plan.main.id
 }
 
-output "app_service_plan_name" {
+output "name" {
   description = "Name of the created App Service Plan"
   value       = azurerm_app_service_plan.main.name
 }
 
-output "app_service_plan_location" {
+output "location" {
   description = "Azure location of the created App Service Plan"
   value       = azurerm_app_service_plan.main.location
 }
 
-output "app_service_plan_max_workers" {
+output "max_workers" {
   description = "Maximum number of workers for the created App Service Plan"
   value       = azurerm_app_service_plan.main.maximum_number_of_workers
 }
