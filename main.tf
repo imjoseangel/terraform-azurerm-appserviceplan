f3fde969a227b7cf47cad548bcbacd442eaa3893
imjoseangel <josea.munoz@gmail.com>
2022-10-09 10:28:44 +0200

    add tflint

--- main.tf
+++ main.tf
@@ -6,8 +6,6 @@ locals {
   location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
 }
 
-data "azurerm_client_config" "current" {}
-
 #---------------------------------------------------------
 # Resource Group Creation or selection - Default is "true"
 #---------------------------------------------------------
--- variables.tf
+++ variables.tf
@@ -1,20 +1,24 @@
 variable "name" {
   description = "Name of Azure Kubernetes service."
+  type        = string
 }
 
 variable "create_resource_group" {
   description = "Whether to create resource group and use it for all networking resources"
   default     = true
+  type        = bool
 }
 
 variable "resource_group_name" {
   description = "A container that holds related resources for an Azure solution"
   default     = "rg-demo-westeurope-01"
+  type        = string
 }
 
 variable "location" {
   description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
   default     = "westeurope"
+  type        = string
 }
 
 variable "prefix" {
@@ -27,12 +31,6 @@ variable "os_type" {
   description = "The kind of the App Service Plan to create. See documentation https://www.terraform.io/docs/providers/azurerm/r/app_service_plan.html#kind"
   type        = string
   default     = "Windows"
-}
-
-variable "reserved" {
-  description = "Flag indicating if App Service Plan should be reserved. Forced to false if 'kind' is 'Windows' and to true if 'kind' is 'Linux'."
-  type        = string
-  default     = "false"
 }
 
 variable "sku_name" {
